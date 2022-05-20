bfbc24b87e18d04ba3007219e7fcf670674133d2
Bruno Terres <brunoeterres@gmail.com>
2022-05-20 10:34:35 -0300

    returning users and permissions per project

--- get_groups.sh
+++ get_groups.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-echo "ID,Name" > groups.csv
+# echo "ID,Name" > groups.csv
 curl -s --header "PRIVATE-TOKEN: $TOKEN" $URL/api/v4/groups?per_page=100 | jq '.[]' | jq '"\(.id) \(.name)"' | sed 's/^"\(.*\)"$/\1/' | sed 's/ \{1,\}/,/g' >> groups.csv
-TEST=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" $URL/api/v4/groups?per_page=100 | jq '.[]' | jq '"\(.id) \(.name)"' | sed 's/^"\(.*\)"$/\1/' | sed 's/ \{1,\}/,/g')
- 
-for T in ${TEST}
+GROUP_LIST=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" $URL/api/v4/groups?per_page=100 | jq '.[]' | jq '"\(.id) \(.name)"' | sed 's/^"\(.*\)"$/\1/' | sed 's/ \{1,\}/,/g')
+
+for G in ${GROUP_LIST}
 do 
-  echo ${T}
-  #curl -s --header "PRIVATE-TOKEN: $TOKEN" $URL/api/v4/groups/${T[0]}/members/all
+  IFS=', ' read -r -a ARRAY <<< "$G"
+  echo "ID, Username, State, Access Lvl" > "${ARRAY[1]}.csv"
+  curl -s --header "PRIVATE-TOKEN: $TOKEN" $URL/api/v4/groups/${ARRAY[0]}/members/all | jq '.[]' | jq '"\(.id) \(.username) \(.state) \(.access_level)"' | sed 's/^"\(.*\)"$/\1/' | sed 's/ \{1,\}/,/g' >> "${ARRAY[1]}.csv"
 done
